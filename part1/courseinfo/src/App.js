f0314cf18f82cb5fa87586bee5c0c8a3ec5d0f94
Ashique Mahmood <ashiquemahmood@outlook.com>
2021-06-15 17:24:19 +1000

    courseinfo
    app for Exercises 1.1.-1.2.

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,67 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.name} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part => <Part name={part.name} exercises={part.exercises} />)}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const totalExercises = props.parts.reduce((a, b) => a + b.exercises, 0);
+  return (
+    <div>
+
+      <p>Number of exercises {totalExercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 
+  {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+export default App
