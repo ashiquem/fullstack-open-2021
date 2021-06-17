dfe5be4f4b8fbde6f8a0eafd8b47558a93529d98
Ashique Mahmood <ashiquemahmood@outlook.com>
2021-06-17 20:24:05 +1000

    exercise 2.11
    updating phonebook to use data from json server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import SearchFilter from './components/SearchFilter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setnewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
   const [filterPersons, setFilterPersons] = useState(persons)
-
+  
+  useEffect(()=>{
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      const fetchedPersons = response.data
+      setPersons(fetchedPersons)
+    })
+  },[])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
