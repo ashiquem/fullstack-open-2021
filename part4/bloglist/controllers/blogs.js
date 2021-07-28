96bdfcc2727f3ba6c9170bfc77c8f971163d9fd3
Ashique Mahmood <ashiquemahmood@outlook.com>
2021-07-28 17:53:45 +1000

    4.13-4.14

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -20,4 +20,37 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+
+  try{
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  }
+  catch(exception)
+  {
+    next(exception)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+
+  try{
+    const body = request.body
+
+    const blog = {
+      title: body.title ,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+    }
+
+    const result = await Blog.findByIdAndUpdate(request.params.id, blog, { new:true, runValidators: true })
+    response.json(result)
+  }
+  catch(exception)
+  {
+    next(exception)
+  }
+})
+
 module.exports = blogsRouter
