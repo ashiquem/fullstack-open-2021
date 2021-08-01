0665a4b26bb7f39f6a8128ea285b4740b99fabe9
Ashique Mahmood <ashiquemahmood@outlook.com>
2021-08-01 17:49:39 +1000

    4.20
    tokenExtractor middleware

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -30,6 +30,7 @@ app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
 
 app.use(constants.URLS.blogs, blogsRouter);
 app.use(constants.URLS.users, usersRouter);
