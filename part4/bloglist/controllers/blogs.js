0e8d1d83b30df02a2a1de116adb6f16bf8396059
Ashique Mahmood <ashiquemahmood@outlook.com>
2021-08-01 18:20:39 +1000

    4.21
    delete authorization

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -38,7 +38,17 @@ blogsRouter.post('/', async (request, response, next) => {
 
 blogsRouter.delete('/:id', async (request, response, next) => {
   try {
-    await Blog.findByIdAndDelete(request.params.id);
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' });
+    }
+    const blog = await Blog.findById(request.params.id);
+
+    if (blog.user.toString() !== decodedToken.id) {
+      return response.status(401).json({ error: 'invalid user' });
+    }
+
+    await blog.delete();
     response.status(204).end();
   } catch (exception) {
     next(exception);
